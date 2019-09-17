d04598b607afbd501033fc2d98e02f482b262f70
Sujay <sujay.ss81@gmail.com>
2019-09-18 00:44:03 +0530

    Added project files with designed homepage

---
+++ resources/views/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Home</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' href="{{asset('bootstrap/css/bootstrap.css')}}">
+    <link rel='stylesheet' type='text/css' href="{{asset('bootstrap/css/style.css' )}}">
+    <script src="{{ asset('bootstrap/js/bootstrap.js' )}}"></script>
+</head>
+<body>
+    <div class="jumbotron-fluid">
+        <h1 class="display-4 text-center">Blood Bank</h1>
+    </div>
+</body>
+</html>
